37a3c97c9ff4959fa1dbb19428d51c9932bb396e
ppfeufer <ppfeufer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-15 14:44:55 +0000

    uebersetzung nachgereicht
    git-svn-id: https://plugins.svn.wordpress.org/2-click-socialmedia-buttons/trunk@463107 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- 2-click-socialmedia-buttons.php
+++ 2-click-socialmedia-buttons.php
@@ -3,12 +3,12 @@
  * Plugin Name: 2 Click Social Media Buttons
  * Plugin URI: http://blog.ppfeufer.de/wordpress-plugin-2-click-social-media-buttons/
  * Description: Fügt die Buttons für Facebook-Like (Empfehlen), Twitter, Flattr und Googleplus dem deutschen Datenschutz entsprechend in euer WordPress ein.
- * Version: 0.18
+ * Version: 0.18.1
  * Author: H.-Peter Pfeufer
  * Author URI: http://ppfeufer.de
  */
 
-define('TWOCLICK_SOCIALMEDIA_BUTTONS_VERSION', '0.18');
+define('TWOCLICK_SOCIALMEDIA_BUTTONS_VERSION', '0.18.1');
 if(!defined('PPFEUFER_FLATTRSCRIPT')) {
 	define('PPFEUFER_FLATTRSCRIPT', 'http://cdn.ppfeufer.de/js/flattr/flattr.js');
 }
